a2c20cc11b761077bd67ef1bea8c6cb2ef5655dd
seastar <2229029156@qq.com>
2022-07-25 21:53:53 +0800

    update IntelligentServer.java

--- src/main/java/micro_service/IntelligentServer.java
+++ src/main/java/micro_service/IntelligentServer.java
@@ -3,7 +3,6 @@ package micro_service;
 import micro_service.config.Config;
 import micro_service.controller.Controller;
 import micro_service.service.SearchService;
-import micro_service.service.QaService;
 
 /**
  * @author sy
@@ -13,7 +12,6 @@ public class IntelligentServer {
 
     public static void main(String ... args) {
         Config.initPort(4567);
-//        Config.initStaticFileLocation("/public");
 //        new Controller(new SearchService(), new QaService()).init();
         new Controller(new SearchService()).init();
     }
