4b994d7e1a009cbd1cf00347879a3314f116c315
seastar <2229029156@qq.com>
2022-08-13 15:31:09 +0800

    update NeoSearch

--- src/main/java/sy/neo/NeoSearch.java
+++ src/main/java/sy/neo/NeoSearch.java
@@ -6,8 +6,10 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.Values;
 import sy.init.neo.NeoPoolUtil;
 import sy.neo.type.EMedicalLabel;
+import sy.neo.type.EMedicalRel;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -86,6 +88,23 @@ public class NeoSearch {
     }
 
     /**
+     * get related disease
+     * @param id
+     * @param query
+     * @param size
+     * @return
+     */
+    public static List<Record> getRelatedDisease(String id, String query, int size) {
+        String cypherQuery = "match(di:" + EMedicalLabel.Disease.name() + "{id:$id1})-[]->(d:" + EMedicalLabel.Department.name() + ") with d call db.index.fulltext.queryNodes('disease', $query) yield node match (node)-[:" + EMedicalRel.BELONGS_TO.name() + "]->(d) where node.id<>$id2 return node.name as name, node.id as id limit $size";
+        Map<String, Object> params = Map.of("id1", id,
+                                            "query", query,
+                                            "id2", id,
+                                            "size", size);
+        List<Record> recordList = runGetCypher(cypherQuery, params);
+        return recordList;
+    }
+
+    /**
      * run cypher and get record
      * @param cypher
      * @return
@@ -106,6 +125,36 @@ public class NeoSearch {
         }
         return recordList;
     }
+
+    /**
+     * run cypher and get record
+     * @param cypher
+     * @param params
+     * @return
+     */
+    public static List<Record> runGetCypher(String cypher, Map<String, Object> params) {
+        List<Record> recordList = null;
+        Driver driver = null;
+        try {
+            driver = NeoPoolUtil.borrowDriver();
+            try(Session session = driver.session()) {
+                if(Optional.ofNullable(params).isPresent()) {
+                    recordList = session.readTransaction(tx ->
+                            tx.run(cypher, params)).list();
+                } else {
+                    recordList = session.readTransaction(tx ->
+                            tx.run(cypher)).list();
+                }
+            }
+        } finally {
+            if(Optional.ofNullable(driver).isPresent()) {
+                NeoPoolUtil.returnDriver(driver);
+            }
+        }
+        return recordList;
+    }
+
+
 
 }
 
