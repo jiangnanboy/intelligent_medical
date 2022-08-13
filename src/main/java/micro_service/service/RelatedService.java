c6f833d55f541b17e45d9401d874387ea23382c0
seastar <2229029156@qq.com>
2022-08-13 15:31:44 +0800

    add related recomm

---
+++ src/main/java/micro_service/service/RelatedService.java
@@ -0,0 +1,24 @@
+package micro_service.service;
+
+import org.neo4j.driver.v1.Record;
+import sy.neo.NeoSearch;
+import utils.CollectionUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author YanShi
+ * @date 2022/8/13 14:45
+ */
+public class RelatedService {
+    public List<Map<String, Object>> getRelatedDisease(String id, String query, int size) {
+        List<Record> recordList = NeoSearch.getRelatedDisease(id, query, size);
+        List<Map<String, Object>> mapNode = CollectionUtil.newArrayList();
+        for(Record record : recordList) {
+            Map<String, Object> map = record.asMap();
+            mapNode.add(map);
+        }
+        return mapNode;
+    }
+}
